317c687622a8973e7196eb1f6cd1e836e46b97ab
backgroundmin <49299170+backgroundmin@users.noreply.github.com>
2023-05-22 15:23:25 +0900

    user 클래스 정의

---
+++ src/User.java
@@ -0,0 +1,17 @@
+package src;
+
+public class User {
+    private String name;
+    private int age;
+    private String phoneNumber;
+    private int remainingAvailabilityDates;
+    private int uniqueNumber;
+
+    public User(String name, int age, String phoneNumber, int remainingAvailabilityDates){
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.remainingAvailabilityDates = remainingAvailabilityDates;
+        this.uniqueNumber = Integer.parseInt(phoneNumber.substring(phoneNumber.length()-4,phoneNumber.length()));
+    }
+}
