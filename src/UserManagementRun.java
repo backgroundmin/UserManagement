2f5b169b33d67f99e7b0f90bdcb9f3c489000f95
backgroundmin <49299170+backgroundmin@users.noreply.github.com>
2023-05-27 23:46:50 +0900

    콘솔 화면으로 관리 제작완료

--- src/UserManagementRun.java
+++ src/UserManagementRun.java
@@ -31,18 +31,6 @@ public class UserManagementRun {
             }
         }
     }
-    public void testUserAdd(){
-        user.add(new User("테스트1",20, "11111111111",20));
-        user.add(new User("테스트2",21, "22222222222",30));
-        user.add(new User("테스트3",22, "33333333333",40));
-        user.add(new User("테스트4",23, "44444444444",50));
-        user.add(new User("테스트5",24, "55555555555",60));
-        user.add(new User("테스트6",25, "66666666666",70));
-        user.add(new User("테스트7",26, "77777777777",80));
-        user.add(new User("테스트8",27, "88888888888",90));
-        user.add(new User("테스트9",28, "11111111111",100));
-        user.add(new User("테스트10",29, "01032803208",3));
-    }
     public void printStartScreen(){
         System.out.println("======회원 관리 시스템 ======");
         System.out.println("1. 회원 생성");
