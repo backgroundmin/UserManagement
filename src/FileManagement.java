a04e611b78db34a06842aa5274b8d03478cf0595
backgroundmin <49299170+backgroundmin@users.noreply.github.com>
2023-05-30 12:28:38 +0900

    파일 입출력 기능 추가

---
+++ src/FileManagement.java
@@ -0,0 +1,41 @@
+package src;
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+public class FileManagement {
+    public void save(ArrayList<User> user){
+        try {
+            String nowTime = String.valueOf(LocalDateTime.now());
+            OutputStream output = new FileOutputStream("data/userInformation.txt");
+            output.write((nowTime+System.getProperty("line.separator")).getBytes());
+            output.write((user.size()+System.getProperty("line.separator")).getBytes());
+            System.out.println("저장중입니다.");
+            String line = System.getProperty("line.separator");
+            for(int i = 0 ; i < user.size() ; i++){
+                User tempUser = user.get(i);
+                String tempInputWord = tempUser.name+" "+tempUser.age+" "+tempUser.phoneNumber+" "+tempUser.remainingAvailabilityDates+"\n";
+                output.write(tempInputWord.replace("\n",line).getBytes());
+            }
+            output.close();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+    public void Load(ArrayList<User> user){
+        try {
+            File file = new File("data/userInformation.txt");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String beforeTime = bufferedReader.readLine();
+            int numberOfUser = Integer.parseInt(bufferedReader.readLine());
+            System.out.println(numberOfUser);
+            for(int i = 0 ; i < numberOfUser ; i++){
+                String [] splitInfo = bufferedReader.readLine().split(" ");
+                user.add(new User(splitInfo[0],Integer.parseInt(splitInfo[1]),splitInfo[2],Integer.parseInt(splitInfo[3])));
+            }
+        } catch (Exception e){
+            e.getStackTrace();
+        }
+    }
+}
--- src/UserManagementRun.java
+++ src/UserManagementRun.java
@@ -6,6 +6,8 @@ public class UserManagementRun {
     public int userCount=0;
     UserManagementFunction userMF = new UserManagementFunction();
     public void run(){
+        FileManagement fileManagement = new FileManagement();
+        fileManagement.Load(user);
         InputInformation inputInfo = new InputInformation();
         int selectNum = 0;
         while(selectNum != 5){
@@ -29,8 +31,23 @@ public class UserManagementRun {
                     break;
             }
         }
+        fileManagement.save(user);
     }
 
+    public void testUserAdd(){
+        user.add(new User("테스트1",20, "01012345678",34));
+        user.add(new User("test2",23, "01010235678",15));
+        user.add(new User("테스트1",20, "11111111111",20));
+        user.add(new User("테스트2",21, "22222222222",30));
+        user.add(new User("테스트3",22, "33333333333",40));
+        user.add(new User("테스트4",23, "44444444444",50));
+        user.add(new User("테스트5",24, "55555555555",60));
+        user.add(new User("테스트6",25, "66666666666",70));
+        user.add(new User("테스트7",26, "77777777777",80));
+        user.add(new User("테스트8",27, "88888888888",90));
+        user.add(new User("테스트9",28, "11111111111",100));
+        user.add(new User("테스트10",29, "01032803208",3));
+    }
     public void printStartScreen(){
         System.out.println("======회원 관리 시스템 ======");
         System.out.println("1. 회원 생성");
